2ff2aab2545e51960649f39a7b2d361477917ee8
chacko jithin ps <jithinchackopayyanat@gmail.com>
2022-05-23 19:07:37 +0530

    added many changes

---
+++ 25 april/javascript/jsproj.js
@@ -0,0 +1,13 @@
+function btnclick(val){
+    // console.log("working")
+      document.getElementById("screen").value = document.getElementById("screen").value+val
+}
+function cleardisplay(){
+    document.getElementById("screen").value=" "
+}
+function equalTo(){
+    var text=document.getElementById("screen").value
+    var res=eval(text)
+    document.getElementById("screen").value=res
+
+}
