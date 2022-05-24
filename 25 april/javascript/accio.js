af27a7433499026d3f4d516be57a797009941ad6
chacko jithin ps <jithinchackopayyanat@gmail.com>
2022-05-24 06:09:18 +0530

    js contents

---
+++ 25 april/javascript/accio.js
@@ -0,0 +1,6 @@
+console.log("Script 1")
+
+document.getElementById("root").innerHTML="<h2>Hey there,This is jithin chacko speaking</h2>"
+document.write("Hey,Whats up?")
+document.getElementById("root2").innerHTML="<h2>Hello</h2>"
+document.write("2+3 is 5")
