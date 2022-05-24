fa0a99efe6b774b04a79740e35d3bff928c05b60
chacko jithin ps <jithinchackopayyanat@gmail.com>
2022-05-24 07:14:20 +0530

    js variables

--- 25 april/javascript/accio.js
+++ 25 april/javascript/accio.js
@@ -4,3 +4,24 @@ document.getElementById("root").innerHTML="<h2>Hey there,This is jithin chacko s
 document.write("Hey,Whats up?")
 document.getElementById("root2").innerHTML="<h2>Hello</h2>"
 document.write("2+3 is 5")
+
+var jit="hello"
+var k=5
+if(k>3){
+    var jit="hey there"
+    console.log(jit)
+    var ponn="good"
+}
+console.log(jit)
+console.log(ponn)
+console.log("-------------------")
+
+let kir="hai"
+let mul=5;
+if(mul>3){
+    let kir="Hai this is kiran"
+    console.log(kir)
+    let hello="jithin"
+    console.log(hello)
+}
+console.log(kir)
