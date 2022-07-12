7ccc4c80e5bfae890f06038590d12e45fe8445d4
chacko jithin ps <jithinchackopayyanat@gmail.com>
2022-07-12 14:53:58 +0530

    sound

--- May 5/sounds/script.js
+++ May 5/sounds/script.js
@@ -2,18 +2,19 @@ const buttons=document.querySelectorAll(".btn")
 
 buttons.forEach(btn=>{
     btn.addEventListener("click",()=>{
-       stopAllsounds();
-       const id=btn.innerText;
-       document.getElementById(id).play()
+        removeAllSounds();
+        const id=btn.innerText;
+        document.getElementById(id).play()
+
     })
 })
-const stopAllsounds=()=>{
+const removeAllSounds=()=>{
     buttons.forEach(btn=>{
         const sound=document.getElementById(btn.innerText)
         sound.pause();
-        sound.currentTime=0;
+        
     })
 }
 document.querySelector(".stop").addEventListener("click",()=>{
-    stopAllsounds();
+    removeAllSounds();
 })
